6f437c0d824da4350fe1eaa69b7f6bd90228d7d5
Emily G <aspentree79@gmail.com>
2019-07-19 01:22:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,6 @@
-# def find_element_index(array, value_to_find)
-# value_index = value_to_find[i]
-# i = 0
-#   while i < array.length do
-#
-#   end
-#   return value_index
-# end
-
 def find_element_index(array, value_to_find)
 array.index(value_to_find)
-# puts value_to_find[i]
 end
-# return value_to_find[i]
 
 
 def find_max_value(array)
